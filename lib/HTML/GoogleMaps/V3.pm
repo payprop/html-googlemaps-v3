1972149b2bc0396e685e2eff7f3fc7f59df02fea
Lee Johnson <lee@givengain.ch>
2018-09-04 09:14:51 +0200

    bump VERSION and Changes for CPAN release

--- lib/HTML/GoogleMaps/V3.pm
+++ lib/HTML/GoogleMaps/V3.pm
@@ -10,7 +10,7 @@ HTML::GoogleMaps::V3 - a simple wrapper around the Google Maps API
 
 =head1 VERSION
 
-0.11
+0.12
 
 =head1 SYNOPSIS
 
@@ -75,7 +75,7 @@ use warnings;
 
 use Template;
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 sub new {
     my ( $class,%opts ) = @_;
